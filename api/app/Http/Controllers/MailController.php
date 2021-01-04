19e64a4e02ac7639b744d8dad3e748afa51747e2
Dmitry Ossipov <dmitry@ossipov.me>
2021-01-04 18:40:04 +0800

    Refactored Business logic from Controller to Service Layer

--- api/app/Http/Controllers/MailController.php
+++ api/app/Http/Controllers/MailController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use App\Http\Requests\MailRequest;
 use App\Jobs\SendEmailJob;
 use App\Models\Mail;
+use App\Services\MailService;
 use Exception;
-use Html2Text\Html2Text;
 use Illuminate\Http\JsonResponse;
 
 class MailController extends Controller
 {
+    protected $mailService;
+
+    public function __construct(MailService $mailService)
+    {
+        $this->mailService = $mailService;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -20,25 +27,13 @@ class MailController extends Controller
     public function index(MailRequest $request): JsonResponse
     {
         if (isset($request->id)) {
-            return response()->json(Mail::findOrFail($request->id));
+            return response()->json(
+                $this->mailService->mailById($request->id)
+            );
         }
-
-        $mail = Mail::select();
-        if (isset($request->subject)) {
-            $mail->containsSubject($request->subject);
-        }
-        if (isset($request->status)) {
-            $mail->hasStatus($request->status);
-        }
-        if (isset($request->to)) {
-            $mail->dedicatedFor($request->to);
-        }
-        if (isset($request->from)) {
-            $mail->sentBy($request->from);
-        }
-        $mail->orderBy('updated_at', 'DESC');
-
-        return response()->json($mail->paginate(10));
+        return response()->json(
+            $this->mailService->filteredListOfMails($request)
+        );
     }
 
     /**
@@ -49,25 +44,7 @@ class MailController extends Controller
      */
     public function store(MailRequest $request): JsonResponse
     {
-        $request = $request->validated();
-
-        $request['text'] = (new Html2Text(preg_replace(
-            '/<li><p>(.*?)<\/p>/i',
-            '<li>$1', $request['html'])
-        ))->getText();
-
-        $attachments = [];
-        $request['attachments'] = $request['attachments'] ?? [];
-        foreach($request['attachments'] as $attachment) {
-            $attachments[] = [
-                'name' => $attachment->getClientOriginalName(),
-                'size' => $attachment->getSize(),
-                'path' => $attachment->store('attachments'),
-            ];
-        }
-        $request['attachments'] = json_encode($attachments);
-        $mail = Mail::create($request);
-
+        $mail = $this->mailService->createMail($request);
         SendEmailJob::dispatch($mail);
         return response()->json($mail, 201);
     }
---
+++ api/app/Services/MailService.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Services;
+
+use App\Models\Mail;
+use Html2Text\Html2Text;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class MailService
+{
+    /**
+     * One mail by Id
+     *
+     * @param $id
+     * @return Mail|Mail[]|Collection|Model|null
+     */
+    public function mailById($id): Mail
+    {
+        return Mail::findOrFail($id);
+    }
+
+    /**
+     * Filtered & paginated & sorted list of mails based on request
+     *
+     * @param $request
+     * @return LengthAwarePaginator
+     */
+    public function filteredListOfMails($request): LengthAwarePaginator
+    {
+        $mail = Mail::select();
+        if (isset($request->subject)) {
+            $mail->containsSubject($request->subject);
+        }
+        if (isset($request->status)) {
+            $mail->hasStatus($request->status);
+        }
+        if (isset($request->to)) {
+            $mail->dedicatedFor($request->to);
+        }
+        if (isset($request->from)) {
+            $mail->sentBy($request->from);
+        }
+        $mail->orderBy('updated_at', 'DESC');
+
+        return $mail->paginate(10);
+    }
+
+    /**
+     * Adds mail to database
+     *
+     * @param $request
+     * @return Mail|Model
+     */
+    public function createMail($request): Mail
+    {
+        $request['text'] = $this->convertHtml2Text($request['html']);
+        $request['attachments'] = $this->extractAttachments($request);
+
+        return Mail::create($request);
+    }
+
+    /**
+     * Converts Html 2 Text removing first occurrence of Paragraph in List's item
+     * to have proper line breaks
+     *
+     * @param $html
+     * @return string
+     */
+    private function convertHtml2Text($html): string
+    {
+        $html = preg_replace('/<li><p>(.*?)<\/p>/i', '<li>$1', $html);
+        return (new Html2Text($html))->getText();
+    }
+
+    private function extractAttachments($request)
+    {
+        $attachments = [];
+        $request['attachments'] = $request['attachments'] ?? [];
+        foreach($request['attachments'] as $attachment) {
+            $attachments[] = [
+                'name' => $attachment->getClientOriginalName(),
+                'size' => $attachment->getSize(),
+                'path' => $attachment->store('attachments'),
+            ];
+        }
+        return json_encode($attachments);
+    }
+}
